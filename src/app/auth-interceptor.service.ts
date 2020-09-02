0bd964b71de80a64c6cfbb5f9bdb7e2a17451f0a
juan.marcos <jamlmarcos@gmail.com>
2020-09-01 21:22:52 -0700

    Added a logging service to log incoming responses. Removed uneccesary logging from auth-interceptor.service.ts.

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -4,13 +4,7 @@ import {tap} from "rxjs/operators";
 
 export class AuthInterceptorService implements HttpInterceptor{
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log('request on its way');
     const modifiedReq = req.clone({headers: req.headers.append('Auth', 'xyz')})
-    return next.handle(modifiedReq).pipe(tap(event => {
-      if(event.type == HttpEventType.Response) {
-        console.log('resp arrived, body data:')
-        console.log(event.body);
-      }
-    }));
+    return next.handle(modifiedReq);
   }
 }
